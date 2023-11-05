957e861f1a51da1172264d5051c645c2762cb1f4
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2023-11-04 19:39:27 -0700

    remove context types

--- src/routes/demos/rendertexture/types.ts
+++
@@ -1,6 +0,0 @@
-import type { Writable } from 'svelte/store';
-
-export type Context = {
-	color: Writable<string>;
-	text: Writable<string>;
-};
--- src/routes/demos/roundedplane/types.ts
+++
@@ -1,7 +0,0 @@
-import type { Writable } from 'svelte/store';
-
-export type Context = {
-	height: Writable<number>;
-	radius: Writable<number>;
-	width: Writable<number>;
-};
