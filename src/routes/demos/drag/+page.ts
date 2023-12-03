ddcfad22afe7451fffb9dff6f531fa3b57b824dd
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2023-12-02 21:36:51 -0800

    add description and title to drag demo

---
+++ src/routes/demos/drag/+page.ts
@@ -0,0 +1,8 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = () => {
+	return {
+		description: 'Demo showing how to drag an object in particular plane.',
+		title: 'Drag Object in a Plane'
+	};
+};
