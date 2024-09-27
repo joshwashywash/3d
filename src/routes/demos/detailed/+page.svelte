9c37bdfeb25f32f0a5856e9ecebb313e2d99f44b
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-09-27 11:09:34 -0700

    remove Detailed. it's in threlte next

--- src/routes/demos/detailed/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { Canvas, T } from '@threlte/core';
-	import Scene from './Scene.svelte';
-</script>
-
-<Canvas>
-	<T.Color args={['black']} attach="background" />
-	<Scene />
-</Canvas>
